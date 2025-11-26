b7ea84865157e97db60e799d9489f4bc0159f3cb
MinSeong Kim <kmslmr98@naver.com>
2025-11-26 16:54:51 +0900

    1-1
    Helloworld, cube

---
+++ Project_E/Assets/1. Scripts/Study.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Study : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        Debug.Log("Hello World!");
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+
