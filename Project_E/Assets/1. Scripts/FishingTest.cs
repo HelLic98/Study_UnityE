85ebdc59ab0ce28ed84db8a30a9d3f82e19fe1f6
MinSeong Kim <kmslmr98@naver.com>
2025-11-27 17:39:00 +0900

    1127

---
+++ Project_E/Assets/1. Scripts/FishingTest.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FishingTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int x = 10;
+        while (x --> 0)
+        {
+            print(x);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Project_E/Assets/1. Scripts/Study.cs
+++ Project_E/Assets/1. Scripts/Study.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class Study : MonoBehaviour
 {
- 
+    public string say = "";
+    private string said ="";
+    string newsay = "Hello World!";
     void Start()
     {
-        Debug.Log("Hello World!");
-
+        Debug.Log(this.say);
     }
 
+    // Update is called once per frame
     void Update()
     {
-        
+     
     }
 }
-
-
