4c360ec343c2f04e599589f3084379be158e3671
MinSeong Kim <kmslmr98@naver.com>
2025-11-28 16:57:38 +0900

    <Unity> 탑 완성
    끼얏호우

--- Project_E/Assets/1. Scripts/FishingTest.cs
+++ Project_E/Assets/1. Scripts/FishingTest.cs
@@ -15,6 +15,12 @@ public class FishingTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+        // a++는 [a = a + 1] 
+
+        // a++ 는[a = a + 1]를 일단 a를 넣고 실행한 다음에 1을 플러스 해준다.
+        // ++a 는 [a = a + 1]를 일단 해주고 실행을 한다.
+
+
     }
 }
