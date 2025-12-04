36e92e31d426db94fcaad030f94363d2b1236430
MinSeong Kim <kmslmr98@naver.com>
2025-12-04 13:37:11 +0900

    하드하게 다 날렸음.

---
+++ Project_E/Assets/01. Scripts/FishingTest.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FishingTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int x = 10;
+        while (x --> 0)
+        {
+            print(x);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        // a++는 [a = a + 1] 
+
+        // a++ 는[a = a + 1]를 일단 a를 넣고 실행한 다음에 1을 플러스 해준다.
+        // ++a 는 [a = a + 1]를 일단 해주고 실행을 한다.
+
+
+    }
+}
